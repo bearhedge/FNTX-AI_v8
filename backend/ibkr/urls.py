b16cf8b9ad06c4aedc0b9699905c6d58c47fa6af
bearhedge <info@bearhedge.com>
2025-05-21 02:13:11 +0800

    Initial baseline: extracted backend and frontend from previous versions

---
+++ backend/ibkr/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import (RangeDataView, SymbolDataView, InstrumentListCreateView, AccountSummaryView, \
+    AuthStatusView, OnboardingView, SystemDataView, TimerDataViewSet, GetHistoryDataView, PlaceOrderView, IBKRTokenView,
+                    DashBoardView)
+
+router = DefaultRouter()
+router.register("onboarding", OnboardingView, "onboarding")
+router.register("instruments", InstrumentListCreateView, "instruments")
+router.register("timer", TimerDataViewSet, "timer")
+router.register("place-order", PlaceOrderView,"place-order")
+router.register("system-data", SystemDataView, "system-data")
+
+urlpatterns = [
+    path('auth-status/', AuthStatusView.as_view(), name='auth_status'),
+    path('account_summary/', AccountSummaryView.as_view(), name='account_summary'),
+    path('symbol_conid',SymbolDataView.as_view(),name='symbol_conid'),
+    path('history_data',GetHistoryDataView.as_view(), name='history_data'),
+    path('dashboard', DashBoardView.as_view(), name="dashboard-view"),
+    path('range',RangeDataView.as_view(),name='Range'),
+    # path('close-position/', ClosePositionView.as_view(), name='Range'),
+    path('get-token', IBKRTokenView.as_view(), name="token"),
+    path("", include(router.urls)),
+]
